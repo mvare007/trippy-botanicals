b311f804e77a80b9f4514cec9591139142bbb252
marcovarela <marcovarela@aquaporservicos.pt>
2024-04-13 11:06:32 +0100

    Refactor configuration settings in config.py

--- config.py
+++ config.py
@@ -1,38 +1,47 @@
 import os
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
     DIR = os.path.dirname(os.path.abspath(__file__))
     TESTING = False
     DEBUG = True
-    SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')
-    DATABASE_NAME = 'trippy'
-    BOOTSTRAP_BOOTSWATCH_THEME = 'Vapor'
+    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
+    DATABASE_NAME = "trippy"
+    BOOTSTRAP_BOOTSWATCH_THEME = "Vapor"
+
 
 class ProductionConfig(Config):
-    ENVIRONMENT = 'production'
+    ENVIRONMENT = "production"
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
-    DATABASE_USER = os.environ.get('DATABASE_USER')
-    DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
+    DATABASE_USER = os.environ.get("DATABASE_USER")
+    DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
+
 
 class DevelopmentConfig(Config):
-    ENVIRONMENT = 'development'
-    SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(os.path.join(base_dir, "tmp", f'{Config.DATABASE_NAME}.db'))
-    DATABASE_USER = 'root'
-    DATABASE_PASSWORD = 'root'
+    ENVIRONMENT = "development"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(
+        os.path.join(base_dir, "tmp", f"{Config.DATABASE_NAME}.db")
+    )
+    DATABASE_USER = "root"
+    DATABASE_PASSWORD = "root"
     SQLALCHEMY_ECHO = True
 
     # Create Development Database if it doesn't exist
     if not os.path.exists(os.path.join(base_dir, "tmp")):
         os.makedirs(os.path.join(base_dir, "tmp"))
-    if not os.path.exists(os.path.join(base_dir, "tmp", f'{Config.DATABASE_NAME}.db')):
-        with open(os.path.join(base_dir, "tmp", f'{Config.DATABASE_NAME}.db'), 'w') as f:
+    if not os.path.exists(os.path.join(base_dir, "tmp", f"{Config.DATABASE_NAME}.db")):
+        with open(
+            os.path.join(base_dir, "tmp", f"{Config.DATABASE_NAME}.db"), "w"
+        ) as f:
             f.write("")
 
+
 class TestingConfig(Config):
-    ENVIRONMENT = 'test'
+    ENVIRONMENT = "test"
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-    DATABASE_USER = 'root'
-    DATABASE_PASSWORD = 'root'
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    DATABASE_USER = "root"
+    DATABASE_PASSWORD = "root"
