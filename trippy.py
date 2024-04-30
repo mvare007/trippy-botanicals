dfe3afcd3928b3aeac6e952c7cde127cc0479b74
marcovarela <marcovarela@aquaporservicos.pt>
2024-04-30 22:01:41 +0100

    Refactor code structure and update dependencies

--- trippy.py
+++ trippy.py
@@ -1,9 +1,11 @@
 import sqlalchemy as sql
 import sqlalchemy.orm as orm
+
 from app import create_app, db
-from app.models import User, ProductCategory, Product, Order, OrderItem
+from app.models import Order, OrderItem, Product, ProductCategory, User
 
 app = create_app()
+
 
 @app.shell_context_processor
 def make_shell_context():
@@ -18,27 +20,28 @@ def make_shell_context():
         "OrderItem": OrderItem,
     }
 
+
 # TODO:
 # * Authorization (login) X
 # * Sql alchemy seed  - user admin and some products X
-# * Backoffice page (only available for admin users) with CRUDS for users, product categories, products, and orders
+# * Backoffice page (only available for admin users) with CRUDS for users, product categories, products, and orders X
 # * handle uploads
 # * Feature daily guessing game - wins points for user
-# * Shopping cart
+# * Shopping cart X
 # * Service to send order data to shipping company
 # * Tests
 
 # TODO: User Stories
 # * As a user, I want to be able to register and login X
-# * As a user, I want to be able to browse products
-# * As a user, I want to be able to add products to my shopping cart
-# * As a user, I want to be able to checkout
-# * As a user, I want to be able to see my order history
-# * As an admin, I want to be able to add products
-# * As an admin, I want to be able to see all orders
-# * As an admin, I want to be able to see all users
-# * As an admin, I want to be able to see all products
-# * As an admin, I want to be able to see all product categories
+# * As a user, I want to be able to browse products X
+# * As a user, I want to be able to add products to my shopping cart X
+# * As a user, I want to be able to checkout X
+# * As a user, I want to be able to see my order history X
+# * As an admin, I want to be able to add products  X
+# * As an admin, I want to be able to see all orders X
+# * As an admin, I want to be able to see all users X
+# * As an admin, I want to be able to see all products X
+# * As an admin, I want to be able to see all product categories X
 
 # TODO: AZURE
 # * Build container
