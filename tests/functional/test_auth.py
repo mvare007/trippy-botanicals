497f2b7730b624d4b2f9f7b89af5bb5bfd07ef03
Marco Varela <marcovarela@aquaporservicos.pt>
2024-05-09 23:28:47 +0100

    Tests

--- tests/functional/test_auth.py
+++ tests/functional/test_auth.py
@@ -1,5 +1,54 @@
 import pytest
 from app.models.user import User
+from factories import UserFactory
+
+def test_login_post(client, database):
+	"""
+		GIVEN a Flask application
+		WHEN a POST request is made to the '/login' route
+		THEN check that the response is valid and the user is redirected to the index page
+	"""
+	user = UserFactory.create()
+	user.set_password("password")
+	database.session.add(user)
+	database.session.commit()
+
+	response = client.post("/login", data=dict(
+		email=user.email,
+		password="password"
+	))
+	redirect_location = response.headers.get('Location')
+
+	assert response.status_code == 302
+	assert redirect_location == "/index"
+
+
+def test_failed_login_post(client, database):
+	"""
+		GIVEN a Flask application
+		WHEN a POST request is made to the '/login' route with invalid credentials
+		THEN check that the response is valid and the user is redirected to the login page
+	"""
+	response = client.post("/login", data=dict(
+		email="invalid@email.com",
+		password="password"
+	))
+	redirect_location = response.headers.get('Location')
+
+	assert response.status_code == 302
+	assert redirect_location == "/login"
+
+
+def test_login_get(client, database):
+	"""
+		GIVEN a Flask application
+		WHEN a GET request is made to the '/login' route
+		THEN check that the response is valid
+	"""
+	response = client.get("/login")
+
+	assert response.status_code == 200
+
 
 def test_register_get(client, database):
 	"""
@@ -8,6 +57,7 @@ def test_register_get(client, database):
 		THEN check that the response is valid and the user is created
 	"""
 	response = client.get("/register")
+
 	assert response.status_code == 200
 
 
@@ -15,7 +65,8 @@ def test_register_post(client, database):
 	"""
 		GIVEN a Flask application
 		WHEN a POST request is made to the '/register' route
-		THEN check that the response is valid and the user is created
+		THEN check that the response is valid
+		AND the user is created AND the user is redirected to the login page
 	"""
 	email = "test@demo.com"
 	response = client.post("/register", data=dict({
@@ -31,6 +82,19 @@ def test_register_post(client, database):
 		"password2": "password",
 	}))
 	user = User.query.filter_by(email=email).first()
+	redirect_location = response.headers.get('Location')
 
 	assert response.status_code == 302
+	assert redirect_location == "/login"
 	assert user is not None
+
+
+	def test_register_get(client, database):
+		"""
+			GIVEN a Flask application
+			WHEN a GET request is made to the '/register' route
+			THEN check that the response is valid
+		"""
+		response = client.get("/register")
+
+		assert response.status_code == 200
