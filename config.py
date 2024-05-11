8d428e30b6c86311480e6f4326963093b6064086
marcovarela <marcovarela@aquaporservicos.pt>
2024-05-11 12:03:32 +0100

    chore: Update Flask environment configuration and Azure db and storage settings

--- config.py
+++ config.py
@@ -1,52 +1,46 @@
 import os
+
 from dotenv import load_dotenv
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(base_dir, ".flask_env"))
+load_dotenv(os.path.join(base_dir, ".flaskenv"))
 
 
-class Config:
+class BaseConfig:
     DIR = os.path.dirname(os.path.abspath(__file__))
     TESTING = False
     DEBUG = True
     SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
-    DATABASE_NAME = "trippy"
     BOOTSTRAP_BOOTSWATCH_THEME = "lux"
     FLASK_ADMIN_SWATCH = "cyborg"
     WTF_CSRF_SECRET_KEY = os.environ.get("WTF_CSRF_SECRET_KEY")
     UPLOAD_FOLDER = f"{base_dir}/app/static/uploads"
     MAX_CONTENT_LENGTH = 10 * 1000 * 1000  # 10MB
     ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+    LOG_LEVEL = "DEBUG"
 
 
-class ProductionConfig(Config):
+class ProductionConfig(BaseConfig):
     ENVIRONMENT = "production"
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
     DATABASE_USER = os.environ.get("DATABASE_USER")
     DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
+    LOG_LEVEL = "WARNING"
 
 
-class DevelopmentConfig(Config):
+class DevelopmentConfig(BaseConfig):
     ENVIRONMENT = "development"
+    DATABASE_NAME = "trippy"
     SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(
-        os.path.join(base_dir, "tmp", f"{Config.DATABASE_NAME}.db")
+        os.path.join(base_dir, "tmp", f"{DATABASE_NAME}.db")
     )
     DATABASE_USER = "root"
     DATABASE_PASSWORD = "root"
-    SQLALCHEMY_ECHO = True # log all the statements issued to stderr.
-
-    # Create Development Database if it doesn't exist
-    if not os.path.exists(os.path.join(base_dir, "tmp")):
-        os.makedirs(os.path.join(base_dir, "tmp"))
-    if not os.path.exists(os.path.join(base_dir, "tmp", f"{Config.DATABASE_NAME}.db")):
-        with open(
-            os.path.join(base_dir, "tmp", f"{Config.DATABASE_NAME}.db"), "w"
-        ) as f:
-            f.write("")
+    SQLALCHEMY_ECHO = True  # log all the statements issued to stderr.
 
 
-class TestingConfig(Config):
+class TestingConfig(BaseConfig):
     ENVIRONMENT = "test"
     TESTING = True
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
