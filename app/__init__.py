cdceb5d0ef9dcf45b88ce23492c2932762430ee8
marcovarela <marcovarela@aquaporservicos.pt>
2024-05-11 12:05:03 +0100

    chore: Update Flask app initialization and database setup
    The code changes in this commit update the Flask app initialization process by adding logger initialization and moving the utils folder to the base directory. Additionally, the database setup functions are refactored to use a new utility class for connecting to an Azure SQL database.

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
-from config import load_config
-from app.main import bp as main_bp
+
 from app.auth import bp as auth_bp
 from app.cli import bp as cli_bp
-from app.extensions import db, migrate, login_manager, bootstrap, register_flask_admin
-from app.models.user import User
-from app.models.product_category import ProductCategory
-from app.models.product import Product
+from app.extensions import bootstrap, db, login_manager, migrate, register_flask_admin
+from app.main import bp as main_bp
 from app.models.order import Order
 from app.models.order_item import OrderItem
+from app.models.product import Product
+from app.models.product_category import ProductCategory
+from app.models.user import User
+from config import load_config
+from utils.database_utils import setup_database_connection
+from utils.logger import init_logger
+
 
 def create_app(test=False):
     app = Flask(__name__)
@@ -16,14 +20,16 @@ def create_app(test=False):
     app.config.from_object(config)
 
     with app.app_context():
+        init_logger(app)
         register_extensions(app)
         register_blueprints(app)
 
     return app
 
+
 def register_extensions(app):
     """Register Flask extensions."""
-    db.init_app(app)
+    setup_database_connection(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
     bootstrap.init_app(app)
