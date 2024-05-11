d2d3189e2f3693d7acf460e84d96ade25fa384a7
marcovarela <marcovarela@aquaporservicos.pt>
2024-05-11 12:04:25 +0100

    Add logger initialization to Flask app

---
+++ utils/logger.py
@@ -0,0 +1,20 @@
+import logging
+from logging.handlers import RotatingFileHandler
+from os import path
+
+from config import base_dir
+
+
+def init_logger(app):
+    log_dir = path.join(base_dir, "log")
+    log_level = app.config["LOG_LEVEL"]
+    file_handler = RotatingFileHandler(
+        path.join(log_dir, "flask_app.log"), maxBytes=16384, backupCount=20
+    )
+    file_formatter = logging.Formatter(
+        "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+    )
+    file_handler.setFormatter(file_formatter)
+    file_handler.setLevel(log_level)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(log_level)
