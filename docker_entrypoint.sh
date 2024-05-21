bc4d8b20e3eeb54de5ada647106c9a00fe147303
Marco Varela <marcovarela@aquaporservicos.pt>
2024-05-21 01:04:58 +0100

    Changed docker entrypoint to only run gunicorn in prod

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # this script is used to boot a Docker container
-while true; do
-    flask db upgrade
-    if [[ "$?" == "0" ]]; then
-        break
-    fi
-    echo Deploy command failed, retrxying in 5 secs...
-    sleep 5
-done
-
 if [ "$FLASK_ENV" == "production" ]; then
+    while true; do
+        flask db upgrade
+        if [[ "$?" == "0" ]]; then
+            break
+        fi
+        echo Deploy command failed, retrxying in 5 secs...
+        sleep 5
+    done
+
     exec gunicorn -b :8000 --access-logfile - --error-logfile - trippy:app
 fi
